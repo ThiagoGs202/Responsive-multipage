26e7a05953266fa83ccb34e353910755cb608c3b
Thiago Gomes <thiago.tgs252@gmail.com>
2023-03-21 15:28:05 -0300

    change icon FAQs JS

--- main.js
+++ main.js
@@ -11,5 +11,13 @@ const faqs = document.querySelectorAll('.faq');
 faqs.forEach(faq => {
     faq.addEventListener('click', () => {
         faq.classList.toggle('open');
+
+        // change icon
+        const icon = faq.querySelector('.faq_icon i');
+        if(icon.className === 'uil uil-plus') {
+            icon.className = "uil uil-minus"
+        } else {
+            icon.className = "uil uil-plus"
+        }
     })
 })
