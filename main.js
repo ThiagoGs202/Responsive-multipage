071ddadca78b887820e135c91a8aabf9f0cb5e6c
Thiago Gomes <thiago.tgs252@gmail.com>
2023-03-21 15:22:03 -0300

    termino das FAQs

--- main.js
+++ main.js
@@ -3,3 +3,13 @@
 window.addEventListener('scroll', () => {
     document.querySelector('nav').classList.toggle('window-scroll', window.scrollY > 0)
 })
+
+// show/hide faq answer
+
+const faqs = document.querySelectorAll('.faq');
+
+faqs.forEach(faq => {
+    faq.addEventListener('click', () => {
+        faq.classList.toggle('open');
+    })
+})
